42b821db8214b13bbb9c65d7a07f390bddeb77c9
Sofian Brabez <sbz@6dev.net>
2019-05-09 14:04:06 +0100

    add alternative compress string implementation w/o collections.OrderedDict

--- compress.py
+++ compress.py
@@ -14,6 +14,8 @@ References:
 
 def compress_string(string):
     """
+    OrderedDict needed to keep the order of the char in the string
+
     Time O(n)
     Space O(n)
     """
@@ -32,9 +34,39 @@ def compress_string(string):
     for char in count:
             if count[char] == 1:
                 compressed += "{0}".format(char)
-            else:    
+            else:
                 compressed += "{0}{1}".format(char, count[char])
 
+    if len(compressed) > len(string):
+        return string
+
+    return compressed
+
+
+def compress_string_alt(string):
+    """
+    Another way to do it without OrderedDict
+
+    Time O(n)
+    Space O(n)
+    """
+    def compress_format_chars(prev_char, count):
+        return "{0}{1}".format(prev_char, count) if count > 1 else prev_char
+
+    if string is None or not string:
+        return string
+
+    compressed = ""
+    count = 0
+    prev_char = string[0]
+    for char in string:
+        if char == prev_char:
+            count += 1
+        else:
+            compressed += compress_format_chars(prev_char, count)
+            prev_char = char
+            count = 1
+    compressed += compress_format_chars(prev_char, count)
     if len(compressed) > len(string):
         return string
 
@@ -48,49 +80,49 @@ class Test(unittest.TestCase):
 
     def test_stream_none(self):
         stream = None
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = None
         self.assertEqual(result, expected)
 
     def test_stream(self):
         stream = "AAAABCCDDDD"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "A4BC2D4"
         self.assertEqual(result, expected)
 
     def test_stream_lowercase(self):
         stream = "AAAABCCDDDD"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "A4BC2D4"
         self.assertEqual(result, expected)
 
     def test_stream_mixedcase(self):
         stream = "AAaaaBCCdDDDD"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "A2a3BC2dD4"
         self.assertEqual(result, expected)
 
     def test_stream_non_need_compress(self):
         stream = "ABC"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "ABC"
         self.assertEqual(result, expected)
 
     def test_stream_empty(self):
         stream = ""
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = ""
         self.assertEqual(result, expected)
 
     def test_stream_one_char(self):
         stream = "Z"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "Z"
         self.assertEqual(result, expected)
 
     def test_stream_two_char(self):
         stream = "ZZ"
-        result = compress_string(stream)
+        result = compress_string_alt(stream)
         expected = "Z2"
         self.assertEqual(result, expected)
 
